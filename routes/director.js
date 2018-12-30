a76b14a11d9815541128362ca16a45cb5ff9da7d
suayb <suaybsimsek58@gmail.com>
2018-12-30 19:19:28 +0300

    director listele endpointi yazıldı

--- routes/director.js
+++ routes/director.js
@@ -17,4 +17,55 @@ router.post('/', (req, res) => {
 
 });
 
+router.get('/', (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true//Filmi olmayan Yönetmenleride getir
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+
+
+    ]);
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+
+});
+
 module.exports = router;
