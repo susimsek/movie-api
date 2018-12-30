916af32bcd9bee620be6b314bd95837778697346
suayb <suaybsimsek58@gmail.com>
2018-12-30 15:09:02 +0300

    movie schemasına date alanı eklendi

--- models/Movie.js
+++ models/Movie.js
@@ -10,7 +10,11 @@ const MovieSchema = new Schema({
     category: String,
     country: String,
     year: Number,
-    imdb_score: Number
+    imdb_score: Number,
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('movie', MovieSchema);
