ec69755a1e6f1bfb8ab19944c38ee5f6b4b06285
suayb <suaybsimsek58@gmail.com>
2018-12-30 18:29:06 +0300

    movie listele between(start year,end year) endpointi yazıldı

--- routes/movie.js
+++ routes/movie.js
@@ -27,6 +27,23 @@ router.get('/top10', (req, res) => {
 
 });
 
+//Between
+router.get('/between/:start_year/:end_year', (req, res) => {
+    const {start_year, end_year} = req.params;
+    const promise = Movie.find({
+        year: {
+            "$gte": parseInt(start_year), "$lte": parseInt(end_year)
+        }
+    });
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+
+});
+
 
 router.get('/:movie_id', (req, res, next) => {
     const movie_id = req.params.movie_id;
