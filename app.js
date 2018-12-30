2f6b92db7f5ba5b2fd563929d72c74b3970903c1
suayb <suaybsimsek58@gmail.com>
2018-12-30 17:08:50 +0300

    movie detay endpointinde filmin bulunamaması durumu için, hata kontrolü yapıldı.

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.json({error: err.message, code: err.code});
 });
 
 module.exports = app;
--- routes/movie.js
+++ routes/movie.js
@@ -15,11 +15,16 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/:movie_id', (req, res) => {
+router.get('/:movie_id', (req, res, next) => {
     const movie_id = req.params.movie_id;
     const promise = Movie.findById(movie_id);
 
     promise.then((data) => {
+        if (!data) {
+            const message = 'The movie was not found';
+            next({message: message, code: 999});
+        }
+
         res.json(data);
     }).catch((err) => {
         res.json(err);
