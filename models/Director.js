934b276a71a4b619a137ac3af5123a8bd44157ea
suayb <suaybsimsek58@gmail.com>
2018-12-30 22:05:48 +0300

    director scheması için validasyon işlemleri yapıldı

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [60, '`{PATH}` (`{VALUE}`),{MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [2, '`{PATH}` (`{VALUE}`),{MINLENGTH} karakterden büyük olmalıdır.']
+    },
+    surname: {
+        type: String,
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [60, '`{PATH}` (`{VALUE}`),{MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [2, '`{PATH}` (`{VALUE}`),{MINLENGTH} karakterden büyük olmalıdır.']
+    },
+    bio: {
+        type: String,
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [1000, '`{PATH}` (`{VALUE}`),{MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [10, '`{PATH}` (`{VALUE}`),{MINLENGTH} karakterden büyük olmalıdır.']
+    },
     createdAt: {
         type: Date,
         default: Date.now
