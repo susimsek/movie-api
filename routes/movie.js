1f954d15655bf159c462a84d286e3e5939d73203
suayb <suaybsimsek58@gmail.com>
2018-12-30 16:42:54 +0300

    movie detay endpointi yazıldı

--- routes/movie.js
+++ routes/movie.js
@@ -15,6 +15,18 @@ router.get('/', (req, res) => {
 
 });
 
+router.get('/:movie_id', (req, res) => {
+    const movie_id = req.params.movie_id;
+    const promise = Movie.findById(movie_id);
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+
+});
+
 router.post('/', (req, res) => {
     const movie = new Movie(req.body);
     const promise = movie.save();
