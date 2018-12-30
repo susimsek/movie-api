1fe6f35e083b78d54ea7a2c4e210f2a0511cb1fe
suayb <suaybsimsek58@gmail.com>
2018-12-30 14:47:06 +0300

    mlabdeki database bağlantıları yapıldı

---
+++ helper/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://root:admin123@ds145434.mlab.com:45434/movie-api', {useNewUrlParser: true});
+
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB:connected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB:Error', err);
+    });
+
+};
