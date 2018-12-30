a5a037711916046a10b408ca41a7d3eef393ca96
suayb <suaybsimsek58@gmail.com>
2018-12-30 22:01:01 +0300

    movie scheması için validasyon işlemleri yapıldı

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [15, '`{PATH}` (`{VALUE}`),{MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [1, '`{PATH}` (`{VALUE}`),{MINLENGTH} karakterden büyük olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: [30, '`{PATH}` (`{VALUE}`),{MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [3, '`{PATH}` (`{VALUE}`),{MINLENGTH} karakterden büyük olmalıdır.']
+    },
+    country: {
+        type: String,
+        maxlength: [30, '`{PATH}` (`{VALUE}`),{MAXLENGTH} karakterden küçük olmalıdır.'],
+        minlength: [3, '`{PATH}` (`{VALUE}`),{MINLENGTH} karakterden büyük olmalıdır.']
+    },
+    year: {
+        type: Number,
+        max: [2040, '`{PATH}` (`{VALUE}`),{MAX} yılından küçük veya eşit olmalıdır.'],
+        min: [1900, '`{PATH}` (`{VALUE}`),{MIN} yılından büyük veya eşit olmalıdır.']
+    },
+    imdb_score: {
+        type: Number,
+        max: [10, '`{PATH}` (`{VALUE}`),{MAX} değerinden küçük veya eşit olmalıdır.'],
+        min: [0, '`{PATH}` (`{VALUE}`),{MIN} değerinden büyük veya eşit olmalıdır.']
+    },
     createdAt: {
         type: Date,
         default: Date.now
