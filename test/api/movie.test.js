deec940a8c8f3d9ddef9fa7fa1199a79ee180a0b
suayb <suaybsimsek58@gmail.com>
2019-01-01 00:11:08 +0300

    movie güncelle endpointinin testi yazıldı

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -84,5 +84,34 @@ describe('/api/movies testi', () => {
 
     });
 
+    describe('/PUT/movie_id movies', () => {
+        it('it should put a movie', (done) => {
+            const movie = {
+                title: '93create',
+                director_id: '5c2935e7a5f70c4f91e602c6',
+                category: 'Suç',
+                country: 'Fransa',
+                year: 1958,
+                imdb_score: 9
+            };
+            chai.request(server)
+                .put(`/api/movies/${movieId}`)
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+                    done();
+                });
+        });
+
+    });
+
 });
 
