17baba28a6632c03c24520023f5204006cde8e86
suayb <suaybsimsek58@gmail.com>
2018-12-31 23:53:03 +0300

    movie ekle endpointinin testi yazıldı

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -33,5 +33,34 @@ describe('/api/movies testi', () => {
 
     });
 
+    describe('/POST movie', () => {
+        it('it should POST a movie', (done) => {
+            const movie = {
+                title: 'Udemy',
+                director_id: '5c2935e7a5f70c4f91e602c6',
+                category: 'Komedi',
+                country: 'Türkiye',
+                year: 1950,
+                imdb_score: 8
+            };
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    done();
+                });
+        });
+
+    });
+
 });
 
