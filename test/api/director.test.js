12e547156919add20c02a4783e53b6b14efcccb3
suayb <suaybsimsek58@gmail.com>
2019-01-01 00:28:35 +0300

    director ekle endpointinin testi yazıldı

--- test/api/director.test.js
+++ test/api/director.test.js
@@ -33,6 +33,31 @@ describe('/api/directors testi', () => {
 
     });
 
+    describe('/POST Director', () => {
+        it('it should POST a director', (done) => {
+            const director = {
+                name: 'ahmet',
+                surname: 'sert',
+                bio: 'ahmet sert kişisel bio'
+            };
+            chai.request(server)
+                .post('/api/directors')
+                .send(director)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('name');
+                    res.body.should.have.property('surname');
+                    res.body.should.have.property('bio');
+                    directorId = res.body._id;
+                    done();
+                });
+        });
+
+    });
+
 
 });
+
 
